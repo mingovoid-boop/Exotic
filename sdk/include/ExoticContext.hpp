4ff425813efa32b8f59c85433fbfd3848039346b
mingovoid-boop <mingovoid@gmail.com>
2026-07-01 11:15:04 -0400

    Refine ExoticContext construction

--- sdk/include/ExoticContext.hpp
+++ sdk/include/ExoticContext.hpp
@@ -3,20 +3,34 @@
 #include "Exotic.hpp"
 
 namespace exotic {
+
 class ExoticContext {
 public:
-ExoticContext() : memory(graph), observer(graph), alignment(graph), prediction(graph), execution(graph), learning(graph), relationship(graph) {}
-Graph graph;
-Config config;
-SDKEventBus events;
-EngineRegistry registry;
-MemoryEngine memory;
-ObserverEngine observer;
-AlignmentEngine alignment;
-PredictionEngine prediction;
-ExecutionEngine execution;
-LearningEngine learning;
-RelationshipEngine relationship;
+    ExoticContext();
+
+    Graph graph;
+    Config config;
+    SDKEventBus events;
+    EngineRegistry registry;
+
+    MemoryEngine memory;
+    ObserverEngine observer;
+    AlignmentEngine alignment;
+    PredictionEngine prediction;
+    ExecutionEngine execution;
+    LearningEngine learning;
+    RelationshipEngine relationship;
 };
+
+inline ExoticContext::ExoticContext()
+    : memory(graph),
+      observer(graph),
+      alignment(graph),
+      prediction(graph),
+      execution(graph),
+      learning(graph),
+      relationship(graph)
+{
 }
 
+} // namespace exotic
