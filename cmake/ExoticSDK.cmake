ca7207355bfb6c703da0da3526749f644ac5f63b
mingovoid-boop <mingovoid@gmail.com>
2026-07-02 23:03:24 -0400

    Add architecture include path

--- cmake/ExoticSDK.cmake
+++ cmake/ExoticSDK.cmake
@@ -2,9 +2,10 @@ file(GLOB_RECURSE EXOTIC_SOURCES foundation/src/*.cpp runtime/src/*.cpp graph/sr
 
 add_library(Exotic STATIC ${EXOTIC_SOURCES})
 
-target_include_directories(Exotic PUBLIC foundation/include runtime/include graph/include memory/include observer/include alignment/include prediction/include execution/include learning/include relationship/include kernel/include sdk/include)
+target_include_directories(Exotic PUBLIC architecture foundation/include runtime/include graph/include memory/include observer/include alignment/include prediction/include execution/include learning/include relationship/include kernel/include sdk/include)
 
 add_executable(HelloExotic ${CMAKE_SOURCE_DIR}/sdk/examples/HelloExotic.cpp)
 target_link_libraries(HelloExotic PRIVATE Exotic)
+
 
 
