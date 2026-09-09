1fedff2423470f2987f2176889145e0801e22c13
mingovoid-boop <mingovoid@gmail.com>
2026-09-09 00:19:38 -0400

    feat(storage): add intention transition migration version

--- CoreRuntime/include/exotic/cognition/storage_schema.hpp
+++ CoreRuntime/include/exotic/cognition/storage_schema.hpp
@@ -1,7 +1,7 @@
 #pragma once
 struct sqlite3;
 namespace exotic::cognition {
-inline constexpr int kCurrentSchemaVersion=2;
+inline constexpr int kCurrentSchemaVersion=3;
 void apply_storage_migrations(sqlite3* db);
 int storage_schema_version(sqlite3* db);
 } // namespace exotic::cognition
