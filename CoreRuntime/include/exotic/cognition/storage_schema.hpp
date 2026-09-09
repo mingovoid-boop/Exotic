1129aeba9be37c359f94d52a0c51216ee6be471e
mingovoid-boop <mingovoid@gmail.com>
2026-09-09 00:31:13 -0400

    feat(cognition): advance schema for intention lifecycle metadata

--- CoreRuntime/include/exotic/cognition/storage_schema.hpp
+++ CoreRuntime/include/exotic/cognition/storage_schema.hpp
@@ -1,7 +1,7 @@
 #pragma once
 struct sqlite3;
 namespace exotic::cognition {
-inline constexpr int kCurrentSchemaVersion=3;
+inline constexpr int kCurrentSchemaVersion=4;
 void apply_storage_migrations(sqlite3* db);
 int storage_schema_version(sqlite3* db);
 } // namespace exotic::cognition
