b598ec2ccb33b6c7b6bf0ab5e47d3fca06d2c3be
mingovoid-boop <mingovoid@gmail.com>
2026-09-08 23:51:57 -0400

    feat(cognition): route idle cognition through agency executive

--- CoreRuntime/include/exotic/cognition/idle_cognition.hpp
+++ CoreRuntime/include/exotic/cognition/idle_cognition.hpp
@@ -1,23 +1,19 @@
 #pragma once
-
+#include "exotic/cognition/agency_executive.hpp"
 #include "exotic/cognition/agent_store.hpp"
 #include "exotic/cognition/free_agent.hpp"
-
+#include <cstdint>
 #include <optional>
 #include <string>
-
 namespace exotic::cognition {
-
+struct IdleAgencyCycle { ThoughtCandidate candidate; AgencyCycle agency; bool persisted{false}; };
 class IdleCognitionScheduler {
  public:
   explicit IdleCognitionScheduler(AgentStore& store) : store_(store) {}
-
   [[nodiscard]] std::optional<ThoughtCandidate> next_candidate() const;
   [[nodiscard]] bool should_run(bool externally_busy) const;
+  [[nodiscard]] std::optional<IdleAgencyCycle> agency_cycle(bool externally_busy,std::int64_t now,bool persist_choice=false);
   void apply_reward(double reward);
-
- private:
-  AgentStore& store_;
+ private: AgentStore& store_;
 };
-
 }  // namespace exotic::cognition
